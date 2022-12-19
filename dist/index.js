662fd85dea673ddd17911decc0f1f2e33473b09a
barrystone <barrystone3183@gmail.com>
2022-12-19 19:28:05 +0800

    tuple & tuple array

--- dist/index.js
+++ dist/index.js
@@ -9,3 +9,18 @@ let ids = [1, 2, 3, 4, 5];
 // ids.push('not number');
 // let ids: number[] = [1, 2, 3, 4, 5, false];
 let arr = [1, true, 'test'];
+// Tuple
+let person = [1, 'barry', true];
+// let person: [number, string, boolean] = ['stone', 'barry', true];
+// Tuple array
+let employees;
+employees = [
+    [1, 'jack'],
+    [2, 'tony'],
+    [3, 'barry']
+];
+// employees = [
+//   [1, 'jack'],
+//   [2, 'tony'],
+//   ['not number', 'barry']
+// ];
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,20 @@ let ids: number[] = [1, 2, 3, 4, 5];
 // ids.push('not number');
 // let ids: number[] = [1, 2, 3, 4, 5, false];
 let arr: any[] = [1, true, 'test'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'barry', true];
+// let person: [number, string, boolean] = ['stone', 'barry', true];
+
+// Tuple array
+let employees: [number, string][];
+employees = [
+  [1, 'jack'],
+  [2, 'tony'],
+  [3, 'barry']
+];
+// employees = [
+//   [1, 'jack'],
+//   [2, 'tony'],
+//   ['not number', 'barry']
+// ];
